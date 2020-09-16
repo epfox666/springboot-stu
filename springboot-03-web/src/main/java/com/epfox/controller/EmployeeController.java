6d9685059b35b286e28e293249a1fa6b85f6a284
username <user@mail.com>
2020-09-16 22:04:32 +0800

    springboot 3

--- springboot-03-web/src/main/java/com/epfox/controller/EmployeeController.java
+++ springboot-03-web/src/main/java/com/epfox/controller/EmployeeController.java
@@ -1,15 +1,55 @@
 package com.epfox.controller;
 
+import com.epfox.dao.DepartmentDao;
 import com.epfox.dao.EmployeeDao;
+import com.epfox.pojo.Department;
+import com.epfox.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
-    public String list(){
-        return "ok";
+
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @RequestMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getall();
+        model.addAttribute("emps",employees);
+        return "emp/list";
     }
+
+    @GetMapping("/emp")
+    public String toAddpage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        System.out.println("save=>"+employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id")Integer id, Model model){
+        System.out.println("id=>"+id);
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp",employee);
+        return "emp/update";
+    }
+
 }
