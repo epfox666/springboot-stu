6b5609eb43ed9009444db69151274096f196439a
username <user@mail.com>
2020-09-17 22:02:52 +0800

    springboot final

--- springboot-03-web/src/main/java/com/epfox/controller/EmployeeController.java
+++ springboot-03-web/src/main/java/com/epfox/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.Collection;
 
 @Controller
@@ -48,8 +49,24 @@ public class EmployeeController {
     public String toUpdateEmp(@PathVariable("id")Integer id, Model model){
         System.out.println("id=>"+id);
         Employee employee = employeeDao.getEmployeeById(id);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
         model.addAttribute("emp",employee);
         return "emp/update";
     }
-
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteEmp(@PathVariable("id")Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
+    }
+    @RequestMapping("/user/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/index.html";
+    }
 }
