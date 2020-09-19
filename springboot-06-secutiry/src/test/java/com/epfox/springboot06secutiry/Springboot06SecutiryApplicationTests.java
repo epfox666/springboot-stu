e19d7b51625b59a2d494675a8b8adabb2690975b
username <user@mail.com>
2020-09-19 22:12:52 +0800

    springboot 06

---
+++ springboot-06-secutiry/src/test/java/com/epfox/springboot06secutiry/Springboot06SecutiryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.epfox.springboot06secutiry;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot06SecutiryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
